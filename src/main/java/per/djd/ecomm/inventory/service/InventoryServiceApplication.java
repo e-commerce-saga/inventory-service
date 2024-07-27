82722081581d96373c79c2f78091c47b998d7eba
Dhruba Dawndawndhruba@gmail.com <dawndhruba@gmail.com>
2024-07-28 00:02:25 +0530

    Initial Commit

---
+++ src/main/java/per/djd/ecomm/inventory/service/InventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package per.djd.ecomm.inventory.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/per/djd/ecomm/inventory/service/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package per.djd.ecomm.inventory.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
